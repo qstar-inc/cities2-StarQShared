f7944530e3e07e838dbbd189c3723c4c3589548c
qstar-inc <qstar.inc@gmail.com>
2025-11-09 03:18:57 +0600

    More Systems

--- Extensions/WorldHelper.cs
+++ Extensions/WorldHelper.cs
@@ -1,17 +1,26 @@
-using System;
-using System.Linq;
+using Game.Common;
+using Game.Notifications;
 using Game.Prefabs;
-using Game.SceneFlow;
+using Game.UI;
+using Game.UI.InGame;
 using Unity.Entities;
 
 namespace StarQ.Shared.Extensions
 {
     public class WorldHelper
     {
-        public static EntityManager entityManager =>
+        public static EntityManager EntityManager =>
             World.DefaultGameObjectInjectionWorld.EntityManager;
 
-        public static PrefabSystem prefabSystem => GetSystem<PrefabSystem>();
+        public static IconCommandSystem IconCommandSystem => GetSystem<IconCommandSystem>();
+        public static ImageSystem ImageSystem => GetSystem<ImageSystem>();
+        public static ModificationBarrier1 ModificationBarrier1 =>
+            GetSystem<ModificationBarrier1>();
+        public static ModificationEndBarrier ModificationEndBarrier =>
+            GetSystem<ModificationEndBarrier>();
+        public static NameSystem NameSystem => GetSystem<NameSystem>();
+        public static PrefabSystem PrefabSystem => GetSystem<PrefabSystem>();
+        public static PrefabUISystem PrefabUISystem => GetSystem<PrefabUISystem>();
 
         public static T GetSystem<T>()
             where T : SystemBase =>
